cc91967c40e1ad3ac59efc3c994e39cc7a6dcbda
Ryan Kwon <rynkwn@gmail.com>
2016-01-11 09:10:31 -0500

    Added messages to beginning of all functions.

---
+++ R/next_trading_day.R
@@ -0,0 +1,23 @@
+#' Next trading day 
+#'
+#' This function gets the next trading day as defined by is.trading.day()
+#'
+#' @param d Date relative to which to get the next trading day. 
+#' 
+#' @return Date The next trading day of date d.
+
+next_trading_day <- function(d) {
+  message(paste0("next_trading_day with parameter: \n", head(d), "\n\n"))
+  stopifnot(inherits(d, "Date"))
+  week <- seq(d + 1, d + 7, by = 1)
+  as.Date(week[is_trading_day(week)][1])
+}
+
+## Check if the date d is a trading day 
+
+is_trading_day <- function(d) {
+  stopifnot(inherits(d, "Date"))
+  
+  ! weekdays(d) %in% c("Saturday", "Sunday")
+  
+}
